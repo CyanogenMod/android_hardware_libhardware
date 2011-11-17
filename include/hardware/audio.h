fb71bb46c5b156b18ef2ead59c3a33d6b3f4c538
Tejas Shikhare <tshikhar@codeaurora.org>
2011-11-16 21:10:53 -0800

    hardware/libhardware: Add APIs to support LPA session
    Change-Id: I0558cafacb25bfffefdfb1bd2afbf2a90f075a08

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -392,6 +392,13 @@ struct audio_hw_device {
                               struct audio_config *config,
                               struct audio_stream_out **stream_out);
 
+#ifdef WITH_QCOM_LPA
+    /** This method creates and opens the audio hardware output session */
+    int (*open_output_session)(struct audio_hw_device *dev, uint32_t devices,
+                              int *format, int sessionId,
+                              struct audio_stream_out **out);
+#endif
+
     void (*close_output_stream)(struct audio_hw_device *dev,
                                 struct audio_stream_out* stream_out);
 
