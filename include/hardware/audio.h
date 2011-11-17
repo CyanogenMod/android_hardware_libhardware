164b3e34861a3d8e233be5a2f6223b420542ac8f
Tejas Shikhare <tshikhar@codeaurora.org>
2011-11-16 21:10:53 -0800

    hardware/libhardware: Add APIs to support LPA session
    Change-Id: I0558cafacb25bfffefdfb1bd2afbf2a90f075a08

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -305,6 +305,13 @@ struct audio_hw_device {
                               uint32_t *sample_rate,
                               struct audio_stream_out **out);
 
+#ifdef WITH_QCOM_LPA
+    /** This method creates and opens the audio hardware output session */
+    int (*open_output_session)(struct audio_hw_device *dev, uint32_t devices,
+                              int *format, int sessionId,
+                              struct audio_stream_out **out);
+#endif
+
     void (*close_output_stream)(struct audio_hw_device *dev,
                                 struct audio_stream_out* out);
 
--- include/hardware/audio_policy.h
+++ include/hardware/audio_policy.h
@@ -126,6 +126,30 @@ struct audio_policy {
      * Audio routing query functions
      */
 
+#ifdef WITH_QCOM_LPA
+    /* request an session appropriate for playback of the supplied stream type and
+     * parameters */
+    audio_io_handle_t (*get_session)(struct audio_policy *pol,
+                                    audio_stream_type_t stream,
+                                    uint32_t format,
+                                    audio_policy_output_flags_t flags,
+                                    int sessionId);
+
+    /* pause session created for LPA Playback */
+    void (*pause_session)(struct audio_policy *pol,
+                          audio_io_handle_t output,
+                          audio_stream_type_t stream);
+
+    /* resume session created for LPA Playback */
+    void (*resume_session)(struct audio_policy *pol,
+                          audio_io_handle_t output,
+                          audio_stream_type_t stream);
+
+    /* release session created for LPA Playback */
+    void (*release_session)(struct audio_policy *pol,
+                          audio_io_handle_t output);
+#endif
+
     /* request an output appriate for playback of the supplied stream type and
      * parameters */
     audio_io_handle_t (*get_output)(struct audio_policy *pol,
@@ -246,6 +270,18 @@ struct audio_policy_service_ops {
                                      uint32_t *pChannels,
                                      uint32_t *pLatencyMs,
                                      audio_policy_output_flags_t flags);
+
+#ifdef WITH_QCOM_LPA
+    audio_io_handle_t (*open_session)(void *service,
+                                     uint32_t *pDevices,
+                                     uint32_t *pFormat,
+                                     audio_policy_output_flags_t flags,
+                                     int32_t stream,
+                                     int32_t sessionId);
+
+    audio_io_handle_t (*close_session)(void *service,
+                                      audio_io_handle_t output);
+#endif
 
     /* creates a special output that is duplicated to the two outputs passed as
      * arguments. The duplication is performed by
