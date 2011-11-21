9e381f18f280f337551522b6799e95533aabc9d0
Naseer Ahmed <naseer@codeaurora.org>
2011-11-20 17:00:37 -0800

    fb: Add support for triple frame buffers
    Change-Id: I95a4e9df1a3afa39b10cd580beff31f1d89f9694
    
    Conflicts:
    
    	include/hardware/fb.h

--- include/hardware/fb.h
+++ include/hardware/fb.h
@@ -64,7 +64,10 @@ typedef struct framebuffer_device_t {
     /* max swap interval supported by this framebuffer */
     const int       maxSwapInterval;
 
-    int reserved[8];
+    /* number of framebuffers */
+    const int       numFramebuffers;
+
+    int reserved[7];
 
     /*
      * requests a specific swap-interval (same definition than EGL)
@@ -126,6 +129,8 @@ typedef struct framebuffer_device_t {
 
     int (*compositionComplete)(struct framebuffer_device_t* dev);
 
+    int (*lockBuffer) (struct framebuffer_device_t* dev, int);
+
     /*
      * This hook is OPTIONAL.
      *
@@ -141,7 +146,11 @@ typedef struct framebuffer_device_t {
      */
     int (*enableScreen)(struct framebuffer_device_t* dev, int enable);
 
-    void* reserved_proc[6];
+    int (*orientationChanged) (struct framebuffer_device_t* dev, int);
+    int (*videoOverlayStarted) (struct framebuffer_device_t* dev, int);
+    int (*enableHDMIOutput) (struct framebuffer_device_t* dev, int);
+    int (*setActionSafeWidthRatio) (struct framebuffer_device_t* dev, float);
+    int (*setActionSafeHeightRatio) (struct framebuffer_device_t* dev, float);
 
 } framebuffer_device_t;
 
