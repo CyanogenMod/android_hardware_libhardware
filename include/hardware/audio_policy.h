codeaurora-unoffical/platform-hardware-libhardware

hardware/libhardware: Add APIs to support LPA session
Change-Id: I0558cafacb25bfffefdfb1bd2afbf2a90f075a08

include/hardware/audio_policy.h
@@ -126,6 +126,28 @@ struct audio_policy {
      * Audio routing query functions
      */
 
+    /* request an session appropriate for playback of the supplied stream type and
+     * parameters */
+    audio_io_handle_t (*get_session)(struct audio_policy *pol,
+                                    audio_stream_type_t stream,
+                                    uint32_t format,
+                                    audio_policy_output_flags_t flags,
+                                    int sessionId);
+
+    /* pause session created for LPA Playback */
+    void (*pause_session)(struct audio_policy *pol,
+                          audio_io_handle_t output,
+                          audio_stream_type_t stream);
+
+    /* resume session created for LPA Playback */
+    void (*resume_session)(struct audio_policy *pol,
+                          audio_io_handle_t output,
+                          audio_stream_type_t stream);
+
+    /* release session created for LPA Playback */
+    void (*release_session)(struct audio_policy *pol,
+                          audio_io_handle_t output);
+
     /* request an output appriate for playback of the supplied stream type and
      * parameters */
     audio_io_handle_t (*get_output)(struct audio_policy *pol,
@@ -246,6 +268,16 @@ struct audio_policy_service_ops {
                                      uint32_t *pChannels,
                                      uint32_t *pLatencyMs,
                                      audio_policy_output_flags_t flags);
+
+    audio_io_handle_t (*open_session)(void *service,
+                                     uint32_t *pDevices,
+                                     uint32_t *pFormat,
+                                     audio_policy_output_flags_t flags,
+                                     int32_t stream,
+                                     int32_t sessionId);
+
+    audio_io_handle_t (*close_session)(void *service,
+                                      audio_io_handle_t output);
 
     /* creates a special output that is duplicated to the two outputs passed as
      * arguments. The duplication is performed by

CyanogenMod/android_hardware_libhardware

include/hardware/audio_policy.h
@@ -126,6 +126,30 @@ struct audio_policy {
      * Audio routing query functions
      */
 
+#ifdef WITH_QCOM_LPA
+    /* request an session appropriate for playback of the supplied stream type and
+     * parameters */
+    audio_io_handle_t (*get_session)(struct audio_policy *pol,
+                                    audio_stream_type_t stream,
+                                    uint32_t format,
+                                    audio_output_flags_t flags,
+                                    int sessionId);
+
+    /* pause session created for LPA Playback */
+    void (*pause_session)(struct audio_policy *pol,
+                          audio_io_handle_t output,
+                          audio_stream_type_t stream);
+
+    /* resume session created for LPA Playback */
+    void (*resume_session)(struct audio_policy *pol,
+                          audio_io_handle_t output,
+                          audio_stream_type_t stream);
+
+    /* release session created for LPA Playback */
+    void (*release_session)(struct audio_policy *pol,
+                          audio_io_handle_t output);
+#endif
+
     /* request an output appropriate for playback of the supplied stream type and
      * parameters */
     audio_io_handle_t (*get_output)(struct audio_policy *pol,
@@ -264,6 +288,18 @@ struct audio_policy_service_ops {
                                      audio_channel_mask_t *pChannelMask,
                                      uint32_t *pLatencyMs,
                                      audio_output_flags_t flags);
+
+#ifdef WITH_QCOM_LPA
+    audio_io_handle_t (*open_session)(void *service,
+                                     uint32_t *pDevices,
+                                     uint32_t *pFormat,
+                                     audio_output_flags_t flags,
+                                     int32_t stream,
+                                     int32_t sessionId);
+
+    audio_io_handle_t (*close_session)(void *service,
+                                      audio_io_handle_t output);
+#endif
 
     /* creates a special output that is duplicated to the two outputs passed as
      * arguments. The duplication is performed by