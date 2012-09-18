b9bfffc5f9d179879511f38326e2af821844f820
codeworkx <codeworkx@cyanogenmod.com>
2012-09-18 20:26:56 +0200

    gralloc: added exynos4 specific usage bits
    Change-Id: Ibd06f153bdfd282993b3f633da82d9a7017aad61

--- include/hardware/gralloc.h
+++ include/hardware/gralloc.h
@@ -98,6 +98,16 @@ enum {
     GRALLOC_USAGE_PRIVATE_2             = 0x40000000,
     GRALLOC_USAGE_PRIVATE_3             = 0x80000000,
     GRALLOC_USAGE_PRIVATE_MASK          = 0xF0000000,
+
+#ifdef EXYNOS4_ENHANCEMENTS
+    /* SAMSUNG */
+    GRALLOC_USAGE_HW_FIMC1              = 0x01000000,
+    GRALLOC_USAGE_HW_ION                = 0x02000000,
+    GRALLOC_USAGE_YUV_ADDR              = 0x04000000,
+
+    /* SEC Private usage , for Overlay path at HWC */
+    GRALLOC_USAGE_HWC_HWOVERLAY         = 0x20000000,
+#endif
 };
 
 /*****************************************************************************/
@@ -188,6 +198,10 @@ typedef struct gralloc_module_t {
     int (*unlock)(struct gralloc_module_t const* module,
             buffer_handle_t handle);
 
+#ifdef EXYNOS4_ENHANCEMENTS
+    int (*getphys) (struct gralloc_module_t const* module,
+            buffer_handle_t handle, void** paddr);
+#endif
 
     /* reserved for future use */
     int (*perform)(struct gralloc_module_t const* module,
