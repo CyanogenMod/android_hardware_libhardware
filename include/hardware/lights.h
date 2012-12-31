e50585d9ab46628ed96222ae88d5a4810191099e
Jason Parker <kornyone@gmail.com>
2012-12-31 00:24:21 -0600

    lights: Adding keypad alt/shift LED light IDs
    in conjunction with the keyboard light patch for
    frameworks/base:
    http://review.cyanogenmod.org/77945
    
    Change-Id: Ie97a80d398d474950340cba60c48ba5904978ec5

--- include/hardware/lights.h
+++ include/hardware/lights.h
@@ -51,6 +51,12 @@ __BEGIN_DECLS
 #define LIGHT_ID_BLUETOOTH          "bluetooth"
 #define LIGHT_ID_WIFI               "wifi"
 
+/*
+ * Additional hardware-specific lights
+ */
+#define LIGHT_ID_CAPS               "caps"
+#define LIGHT_ID_FUNC               "func"
+
 /* ************************************************************************
  * Flash modes for the flashMode field of light_state_t.
  */
