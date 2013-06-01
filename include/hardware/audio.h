bece41550eea19217a3995123679825fe8ef42b5
maclaw <maclaw@maclaw.pl>
2013-06-01 19:59:34 +0200

    libhardware: Enable broadcast stream for audio hardware with  SAMSUNG_STE
    Change-Id: Iaf3b58fdb6c888c1098ae8359a9c27efa7f81940

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -570,7 +570,7 @@ struct audio_hw_device {
     void (*close_output_stream)(struct audio_hw_device *dev,
                                 struct audio_stream_out* stream_out);
 
-#ifdef QCOM_HARDWARE
+#if defined (QCOM_HARDWARE) || defined (STE_SAMSUNG_HARDWARE)
     /** This method creates and opens the audio hardware output
      *  for broadcast stream */
     int (*open_broadcast_stream)(struct audio_hw_device *dev, uint32_t devices,
