2f061319d6a020d73e0325db775edc79587976de
Mingming Yin <mingming@codeaurora.org>
2013-06-03 16:41:08 -0700

    audio: Add QCOM feature support for frame size
    - Add support for QCOM audio formats in audio_stream_frame_size
    - Send correct channel count for voice call recording
    - add support for VOIP
    
    Change-Id: I7a2f5a25c7571b3abf4cedb9d9104681304d1629

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -318,8 +318,43 @@ typedef struct audio_stream_in audio_stream_in_t;
 static inline size_t audio_stream_frame_size(const struct audio_stream *s)
 {
     size_t chan_samp_sz;
+    uint32_t chan_mask = s->get_channels(s);
+    int format = s->get_format(s);
+    char *tmpparam;
+    int isParamEqual;
 
-    switch (s->get_format(s)) {
+    if(!s)
+        return 0;
+
+    if (audio_is_input_channel(chan_mask)) {
+        chan_mask &= (AUDIO_CHANNEL_IN_STEREO | \
+                      AUDIO_CHANNEL_IN_MONO | \
+                      AUDIO_CHANNEL_IN_5POINT1);
+    }
+
+    tmpparam = s->get_parameters(s, "voip_flag");
+    isParamEqual = !strncmp(tmpparam,"voip_flag=1", sizeof("voip_flag=1"));
+    free(tmpparam);
+    if(isParamEqual) {
+        if(format != AUDIO_FORMAT_PCM_8_BIT)
+            return popcount(chan_mask) * sizeof(int16_t);
+        else
+            return popcount(chan_mask) * sizeof(int8_t);
+    }
+
+    switch (format) {
+    case AUDIO_FORMAT_AMR_NB:
+        chan_samp_sz = 32;
+        break;
+    case AUDIO_FORMAT_EVRC:
+        chan_samp_sz = 23;
+        break;
+    case AUDIO_FORMAT_QCELP:
+        chan_samp_sz = 35;
+        break;
+    case AUDIO_FORMAT_AMR_WB:
+        chan_samp_sz = 61;
+        break;
     case AUDIO_FORMAT_PCM_16_BIT:
         chan_samp_sz = sizeof(int16_t);
         break;
@@ -329,7 +364,7 @@ static inline size_t audio_stream_frame_size(const struct audio_stream *s)
         break;
     }
 
-    return popcount(s->get_channels(s)) * chan_samp_sz;
+    return popcount(chan_mask) * chan_samp_sz;
 }
 
 
