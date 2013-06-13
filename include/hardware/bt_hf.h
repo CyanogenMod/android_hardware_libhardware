2948eecd3ea9d1e61c95406eac58113958fef876
Nitin Srivastava <nitinsr@codeaurora.org>
2013-06-13 11:47:21 +0530

    Bluetooth: Added new prototype of HFP feature bitmask.
    This function will allow to let the BT stack use
    the features supported by the Bluetooth HFP app.
    
    CRs-Fixed: 482189
    Change-Id: I9d015562e310ed2901ada91e692083dde7ac4614

--- include/hardware/bt_hf.h
+++ include/hardware/bt_hf.h
@@ -1,4 +1,6 @@
-/*
+/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -228,6 +230,9 @@ typedef struct {
      * Register the BtHf callbacks
      */
     bt_status_t (*init)( bthf_callbacks_t* callbacks, int max_hf_clients);
+
+    /** Set the feature bitmask */
+    bt_status_t (*init_features)( int feature_bitmask );
 
     /** connect to headset */
     bt_status_t (*connect)( bt_bdaddr_t *bd_addr );
