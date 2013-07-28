b5b37beee17e16f7952464ddf893ecb18a025ed7
sportsstar89 <sjpirri@gmail.com>
2013-07-28 14:58:19 -0400

    camera: add ifdef to avoid conflicts
    * add ifdef to camera modules so devices that use a custom module with the same name can compile
    
    Change-Id: I17407b1631b103095e9c8d6c4a9cd20b6fc07f50

--- modules/Android.mk
+++ modules/Android.mk
@@ -1,2 +1,5 @@
-hardware_modules := gralloc hwcomposer audio nfc nfc-nci local_time power usbaudio audio_remote_submix camera
+hardware_modules := gralloc hwcomposer audio nfc nfc-nci local_time power usbaudio audio_remote_submix
+ifneq ($(TARGET_USE_CUSTOM_WRAPPER),true)
+hardware_modules += camera
+endif
 include $(call all-named-subdir-makefiles,$(hardware_modules))
