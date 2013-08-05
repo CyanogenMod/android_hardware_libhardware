051951151214a94cb3b9d9e9bf87c59185ce0db9
Steve Kondik <shade@chemlab.org>
2013-08-05 00:21:06 -0700

    audio: Fix merge issues
    Change-Id: If72942b042ab1cebd1788644954fd13eee028e49

--- modules/audio/audio_hw.c
+++ modules/audio/audio_hw.c
@@ -301,6 +301,11 @@ static int adev_set_master_mute(struct audio_hw_device *dev, bool muted)
 {
     return -ENOSYS;
 }
+
+static int adev_get_master_mute(struct audio_hw_device *dev, bool *muted)
+{
+    return -ENOSYS;
+}
 #endif
 
 static int adev_set_mode(struct audio_hw_device *dev, audio_mode_t mode)
@@ -404,6 +409,7 @@ static int adev_open(const hw_module_t* module, const char* name,
 #ifndef ICS_AUDIO_BLOB
     adev->device.get_master_volume = adev_get_master_volume;
     adev->device.set_master_mute = adev_set_master_mute;
+    adev->device.get_master_mute = adev_get_master_mute;
 #endif
     adev->device.set_mode = adev_set_mode;
     adev->device.set_mic_mute = adev_set_mic_mute;
