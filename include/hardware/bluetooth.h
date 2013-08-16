b318355942d9c0a81df5225da03298588d4d09ec
Ashwini Munigala <AshwiniM@codeaurora.org>
2013-08-16 14:42:26 +0530

    Bluetooth: Support MAP Client role on Bluedroid.
    Implementation of MAP Client (MCE) role on
    Bluedroid stack. Include MAP Client Interface ID.
    
    Change-Id: Icdf11b7f09a161e35aa3aa68b9b5fa315e18fd65
    
    CRs-fixed: 539274

--- include/hardware/bluetooth.h
+++ include/hardware/bluetooth.h
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution
+ *
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -42,6 +45,7 @@ __BEGIN_DECLS
 #define BT_PROFILE_SOCKETS_ID "socket"
 #define BT_PROFILE_HIDHOST_ID "hidhost"
 #define BT_PROFILE_PAN_ID "pan"
+#define BT_PROFILE_MAP_CLIENT_ID "map_client"
 
 #define BT_PROFILE_GATT_ID "gatt"
 #define BT_PROFILE_AV_RC_ID "avrcp"
