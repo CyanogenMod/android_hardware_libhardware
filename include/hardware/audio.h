17a5ac8e7c6e00f4f6991ade2a0d5dbb387bc9e2
Preetam Singh Ranawat <apranawat@codeaurora.org>
2013-10-01 17:12:52 +0530

    libhardware: Add APIs to support DirectTrack
     -Add APIs in HAL to support Low Power Audio
      playback and Tunnel Audio playback.
    
    Change-Id: Ic9d595e599c3222e2535745a8f1ebfb018ef75ed
    (cherry picked from commit 4af9447d1c37f117463e2855fec845adaa299737)
    (cherry picked from commit 6c2165c00805016ad5b4c2af3ac4ebfb49fd9170)
    (cherry picked from commit c15cb2d03a371314c922588562a3e8a9f7238c08)

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -155,6 +157,14 @@ struct audio_config {
 };
 typedef struct audio_config audio_config_t;
 
+/** Structure to save buffer information for applying effects for
+ *  LPA buffers */
+struct buf_info {
+    int bufsize;
+    int nBufs;
+    int **buffers;
+};
+
 /* common audio stream parameters and operations */
 struct audio_stream {
 
@@ -302,6 +312,15 @@ struct audio_stream_out {
                                uint32_t *dsp_frames);
 
     /**
+     * start audio data rendering
+     */
+    int (*start)(struct audio_stream_out *stream);
+
+    /**
+     * stop audio data rendering
+     */
+    int (*stop)(struct audio_stream_out *stream);
+    /**
      * get the local time at which the next write to the audio driver will be presented.
      * The units are microseconds, where the epoch is decided by the local audio HAL.
      */
@@ -382,6 +401,28 @@ struct audio_stream_out {
     int (*get_presentation_position)(const struct audio_stream_out *stream,
                                uint64_t *frames, struct timespec *timestamp);
 
+    /**
+    * return the current timestamp after quering to the driver
+     */
+    int (*get_time_stamp)(const struct audio_stream_out *stream,
+                               uint64_t *time_stamp);
+    /**
+    * EOS notification from HAL to Player
+     */
+    int (*set_observer)(const struct audio_stream_out *stream,
+                               void *observer);
+    /**
+     * Get the physical address of the buffer allocated in the
+     * driver
+     */
+    int (*get_buffer_info) (const struct audio_stream_out *stream,
+                                struct buf_info **buf);
+    /**
+     * Check if next buffer is available. Waits until next buffer is
+     * available
+     */
+    int (*is_buffer_available) (const struct audio_stream_out *stream,
+                                     int *isAvail);
 };
 typedef struct audio_stream_out audio_stream_out_t;
 
@@ -603,6 +644,16 @@ static inline int audio_hw_device_close(struct audio_hw_device* device)
 }
 
 
+#ifdef __cplusplus
+/**
+ *Observer class to post the Events from HAL to Flinger
+*/
+class AudioEventObserver {
+public:
+    virtual ~AudioEventObserver() {}
+    virtual void postEOS(int64_t delayUs) = 0;
+};
+#endif
 __END_DECLS
 
 #endif  // ANDROID_AUDIO_INTERFACE_H
