b4a92271738e35c791647a096d169821477e92e1
Kiran Kandi <kkandi@codeaurora.org>
2013-10-17 15:49:54 -0700

    audio: add set_parameters for listen hal
    Add listen hal specific set parameters function so that Listen
    service can call Listen hal directly, by passing audio hal. This
    is needed since set_parameters call in audio hal does not guarantee the
    correct return value is returned for any parameters set.
    
    Change-Id: I647b9e7f841c8157e4eb597655166ea863a00925

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -30,6 +30,9 @@
 #include <hardware/hardware.h>
 #include <system/audio.h>
 #include <hardware/audio_effect.h>
+#ifdef AUDIO_LISTEN_ENABLED
+#include <listen_types.h>
+#endif
 
 __BEGIN_DECLS
 
@@ -711,6 +714,27 @@ struct audio_hw_device {
      * method may leave it set to NULL.
      */
     int (*get_master_mute)(struct audio_hw_device *dev, bool *mute);
+#endif
+
+#ifdef AUDIO_LISTEN_ENABLED
+    /** This method opens the listen session and returns a handle */
+    int (*open_listen_session)(struct audio_hw_device *dev,
+                               struct listen_session** handle);
+
+    /** This method closes the listen session  */
+    int (*close_listen_session)(struct audio_hw_device *dev,
+                                struct listen_session* handle);
+
+    /** This method sets the mad observer callback  */
+    int (*set_mad_observer)(struct audio_hw_device *dev,
+                            listen_callback_t cb_func);
+
+    /*  This method is used for setting listen hal specfic parameters.
+     *  If multiple paramets are set in one call and setting any one of them
+     *  fails it will return failure.
+     */
+    int (*listen_set_parameters)(struct audio_hw_device *dev,
+                                 const char *kv_pairs);
 #endif
 };
 typedef struct audio_hw_device audio_hw_device_t;
