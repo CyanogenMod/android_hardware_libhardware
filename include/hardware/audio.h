488329e9c6b78f31de94cfeebda190c4f4277da0
iiD4x <t2pksa@gmail.com>
2014-04-16 00:01:18 +0300

    Fix typo
    Change-Id: I7983cc44ba67669ef9e8cc42af5acd8cbda9ac9c

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -148,7 +148,7 @@ __BEGIN_DECLS
 #define AUDIO_OFFLOAD_CODEC_WMA_BIT_PER_SAMPLE "music_offload_wma_bit_per_sample"
 #define AUDIO_OFFLOAD_CODEC_WMA_CHANNEL_MASK "music_offload_wma_channel_mask"
 #define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION "music_offload_wma_encode_option"
-#define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION1"music_offload_wma_encode_option1"
+#define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION1 "music_offload_wma_encode_option1"
 #define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION2 "music_offload_wma_encode_option2"
 #define AUDIO_OFFLOAD_CODEC_FORMAT  "music_offload_codec_format"
 
