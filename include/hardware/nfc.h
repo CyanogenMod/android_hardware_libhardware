80c4de6064fc6262a9160a87dba25916eb727cd4
Jizhou Liao <jizhou.liao@nxp.com>
2014-08-01 10:43:03 -0700

    Add Nfc HAL module ID for NXP NCI PN54x chips
    Change-Id: I4164cbb123050ba44758eb8ac26e6c22df34b8b9

--- include/hardware/nfc.h
+++ include/hardware/nfc.h
@@ -54,6 +54,7 @@ __BEGIN_DECLS
  */
 #define NFC_NCI_HARDWARE_MODULE_ID "nfc_nci"
 #define NFC_NCI_BCM2079X_HARDWARE_MODULE_ID "nfc_nci.bcm2079x"
+#define NFC_NCI_NXP_PN54X_HARDWARE_MODULE_ID "nfc_nci.pn54x"
 #define NFC_NCI_CONTROLLER "nci"
 
 /*
