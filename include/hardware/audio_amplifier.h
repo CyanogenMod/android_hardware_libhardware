c5912f4eb10386d25c10080a62c3d24142ef3d39
Ethan Chen <intervigil@gmail.com>
2015-06-07 11:34:30 -0700

    audio: Add audio amplifier HAL
    Change-Id: Iad42bcc183dda7dba62a67959fb793e089c5d46e

---
+++ include/hardware/audio_amplifier.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CM_AUDIO_AMPLIFIER_INTERFACE_H
+#define CM_AUDIO_AMPLIFIER_INTERFACE_H
+
+#include <stdint.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+#include <hardware/audio.h>
+#include <hardware/hardware.h>
+
+#include <system/audio.h>
+
+__BEGIN_DECLS
+
+#define AMPLIFIER_MODULE_API_VERSION_0_1 HARDWARE_MODULE_API_VERSION(0, 1)
+
+#define AMPLIFIER_HARDWARE_MODULE_ID "audio_amplifier"
+
+typedef struct amplifier_module {
+    struct hw_module_t common;
+
+    /**
+     * Initialize amplifier hardware
+     *
+     * Should only occur once on audio HAL initialization
+     */
+    int (*init)(struct amplifier_module *module);
+
+    /**
+     * Notify amplifier module of current input/output devices
+     *
+     * This function should handle both input and output devices.
+     */
+    int (*set_devices)(struct amplifier_module *module, int devices);
+
+    /**
+     * Notify amplifier module about current audio mode
+     */
+    int (*set_mode)(struct amplifier_module *module, audio_mode_t mode);
+
+    /**
+     * Notify amplifier module that an output stream has started
+     */
+    int (*stream_start)(struct amplifier_module *module,
+            struct audio_stream_out *stream, bool offload);
+
+    /**
+     * Notify amplifier module that an output stream has stopped
+     */
+    int (*stream_standby)(struct amplifier_module *module,
+            struct audio_stream_out *stream);
+
+    /**
+     * Shut down amplifier hardware
+     */
+    int (*shutdown)(struct amplifier_module *module);
+} amplifier_module_t;
+
+__END_DECLS
+
+#endif  // CM_AUDIO_AMPLIFIER_INTERFACE_H
---
+++ modules/audio/audio_amplifier.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "amplifier_default"
+//#define LOG_NDEBUG 0
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#include <cutils/log.h>
+
+#include <hardware/audio_amplifier.h>
+#include <hardware/hardware.h>
+
+static int amp_init(struct amplifier_module *module)
+{
+    return 0;
+}
+
+static int amp_set_devices(struct amplifier_module *module, int devices)
+{
+    return 0;
+}
+
+static int amp_set_mode(struct amplifier_module *module, audio_mode_t mode)
+{
+    return 0;
+}
+
+static int amp_stream_start(struct amplifier_module *module,
+        struct audio_stream_out *stream, bool offload)
+{
+    return 0;
+}
+
+static int amp_stream_standby(struct amplifier_module *module,
+        struct audio_stream_out *stream)
+{
+    return 0;
+}
+
+static int amp_shutdown(struct amplifier_module *module)
+{
+    return 0;
+}
+
+static struct hw_module_methods_t hal_module_methods = {
+    .open = NULL,
+};
+
+struct amplifier_module_t HAL_MODULE_INFO_SYM = {
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .module_api_version = AMPLIFIER_MODULE_API_VERSION_0_1,
+        .hal_api_version = HARDWARE_HAL_API_VERSION,
+        .id = AMPLIFIER_HARDWARE_MODULE_ID,
+        .name = "Default audio amplifier HAL",
+        .author = "The CyanogenMod Open Source Project",
+        .methods = &hal_module_methods,
+        .dso = NULL,
+        .reserved = {0},
+    },
+    .init = amp_init,
+    .set_devices = amp_set_devices,
+    .set_mode = amp_set_mode,
+    .stream_start = amp_stream_start,
+    .stream_standby = amp_stream_standby,
+    .shutdown = amp_shutdown,
+};
