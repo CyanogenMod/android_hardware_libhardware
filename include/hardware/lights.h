e8f4b8c1eab321fd72df2699b7b03149cbaf7ea7
AdrianDC <radian.dc@gmail.com>
2015-07-14 10:48:37 +0200

    Multiple LEDs [1/3]: Illumination Bars support
    Implement the support of a multiple LEDs settings.
    
    The setting is deactivated by default
    and will be ignored by the unimplemented phones.
    Current LibLights will simply not use the new variable.
    
    Changes includes :
      frameworks/base
      hardware/libhardware
      packages/Apps/Settings
    
    Change-Id: I5242d1999f351909d284b88a8f14accaeb24ba49
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- include/hardware/lights.h
+++ include/hardware/lights.h
@@ -88,6 +88,11 @@ __BEGIN_DECLS
 #define BRIGHTNESS_MODE_SENSOR      1
 
 /**
+ * Light mode allows multiple LEDs
+ */
+#define LIGHT_MODE_MULTIPLE_LEDS    0x01
+
+/**
  * The parameters that can be set for a given light.
  *
  * Not all lights must support all parameters.  If you
@@ -122,6 +127,12 @@ struct light_state_t {
      * Currently the values are BRIGHTNESS_MODE_USER and BRIGHTNESS_MODE_SENSOR.
      */
     int brightnessMode;
+
+    /**
+     * Define the LEDs modes (multiple, ...).
+     * See the LIGHTS_MODE_* mask constants.
+     */
+    unsigned int ledsModes;
 };
 
 struct light_device_t {
