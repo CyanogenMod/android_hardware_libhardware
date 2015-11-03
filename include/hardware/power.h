a14736f5986bd39fe919bc70e6330eb1fe4e6df9
Steve Kondik <steve@cyngn.com>
2015-11-02 19:35:35 -0800

    hardware: Add new getFeature() function to Power HAL
     * The current use case for this is to query the power HAL for
       the number of supported profiles.
    
    Change-Id: I0b4137dda8d98d167d9c6446072bdbe1245ae2f5

--- include/hardware/power.h
+++ include/hardware/power.h
@@ -56,7 +56,8 @@ typedef enum {
 } power_hint_t;
 
 typedef enum {
-    POWER_FEATURE_DOUBLE_TAP_TO_WAKE = 0x00000001
+    POWER_FEATURE_DOUBLE_TAP_TO_WAKE = 0x00000001,
+    POWER_FEATURE_SUPPORTED_PROFILES = 0x00001000
 } feature_t;
 
 /**
@@ -158,6 +159,12 @@ typedef struct power_module {
      *
      */
     void (*setFeature)(struct power_module *module, feature_t feature, int state);
+
+    /*
+     * (*getFeature) is called to get the current value of a particular
+     * feature or capability from the hardware or PowerHAL
+     */
+    int (*getFeature)(struct power_module *module, feature_t feature);
 
 } power_module_t;
 
