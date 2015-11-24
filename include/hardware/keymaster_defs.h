4b08b7ec6ba891341432c377463b4d401bae80cb
Mao Li <maol@codeaurora.org>
2015-11-24 13:35:26 +0800

    keymaster: Add SOTER tags and API definations
    Add SOTER tags and API definations according to SOTER
    specification.
    
    RM-290
    
    Change-Id: I20ba0f51d8825a326b51f47ef4d2a3e4f60e2172

--- include/hardware/keymaster_defs.h
+++ include/hardware/keymaster_defs.h
@@ -124,6 +124,27 @@ typedef enum {
                                                  authentication has been performed.  Structure
                                                  defined in hw_auth_token_t in hw_auth_token.h. */
     KM_TAG_MAC_LENGTH = KM_UINT | 1003,       /* MAC or AEAD authentication tag length in bits. */
+
+    /* Tags used only for SOTER */
+    /* Tags used only to check if the key is for SOTER */
+    KM_TAG_SOTER_IS_FROM_SOTER = KM_BOOL | 11000,
+    /* Attach signature signed with ATTK[pri] while exporting public key */
+    KM_TAG_SOTER_IS_AUTO_SIGNED_WITH_ATTK_WHEN_GET_PUBLIC_KEY = KM_BOOL | 11001,
+    /* Attach signature signed with specified private key while exporting public key */
+    KM_TAG_SOTER_IS_AUTO_SIGNED_WITH_COMMON_KEY_WHEN_GET_PUBLIC_KEY = KM_BOOL | 11002,
+    /* keyalias for the keypair of KM_TAG_SOTER_IS_AUTO_SIGNED_WITH_COMMON_KEY_WHEN_GET_PUBLIC_KEY */
+    KM_TAG_SOTER_AUTO_SIGNED_COMMON_KEY_WHEN_GET_PUBLIC_KEY = KM_BYTES | 11003,
+    /* Attach counter while exporting publick key */
+    KM_TAG_SOTER_AUTO_ADD_COUNTER_WHEN_GET_PUBLIC_KEY = KM_BOOL | 11004,
+    /* Attach secmsg(TEE_Name, TEE_Version, Fingerprint_Sensor_Name, Fingerprint_Sensor_Version)
+       fingerprint_id and counter while signing */
+    KM_TAG_SOTER_IS_SECMSG_FID_COUNTER_SIGNED_WHEN_SIGN = KM_BOOL | 11005,
+    /* use and set ATTK index to next backup ATTK */
+    KM_TAG_SOTER_USE_NEXT_ATTK = KM_BOOL | 11006,
+    /* attach soter uid */
+    KM_TAG_SOTER_UID = KM_UINT | 11007,
+    /* attach key blob of KM_TAG_SOTER_AUTO_SIGNED_COMMON_KEY_WHEN_GET_PUBLIC_KEY if needed */
+    KM_TAG_SOTER_AUTO_SIGNED_COMMON_KEY_WHEN_GET_PUBLIC_KEY_BLOB = KM_BYTES | 11008,
 } keymaster_tag_t;
 
 /**
