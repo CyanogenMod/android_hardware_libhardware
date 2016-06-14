142c54a1503f46f0912fe77eb773a2148f4427f8
Ajay Panicker <apanicke@google.com>
2016-06-13 23:37:37 -0700

    Add guest mode functionality (1/3)
    Add a flag to enable() to start Bluetooth in restricted
    mode. In restricted mode, all devices that are paired during
    restricted mode are deleted upon leaving restricted mode.
    Right now restricted mode is only entered while a guest
    user is active
    
    CYNGNOS-3020
    Bug: 27410683
    Change-Id: I994a2933fd60301927ff2df65da634f81d4c9428
    (cherry picked from commit 4e10135ef499073711944860ccf9a7c43b8bf6ac)

--- include/hardware/bluetooth.h
+++ include/hardware/bluetooth.h
@@ -486,7 +486,7 @@ typedef struct {
     int (*initq)(bt_callbacks_t* callbacks);
 
     /** Enable Bluetooth. */
-    int (*enable)(void);
+    int (*enable)(bool guest_mode);
 
     /** Disable Bluetooth. */
     int (*disable)(void);
